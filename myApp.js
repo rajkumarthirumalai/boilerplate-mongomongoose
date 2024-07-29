c5652dc07469616ee0a5d7a05fbb0be0b5078721
Rajkumar <rajkumarrajkumar061@gmail.com>
2024-07-29 17:07:03 +0000

    mongoose connection

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let Person;
 
 const createAndSavePerson = (done) => {
