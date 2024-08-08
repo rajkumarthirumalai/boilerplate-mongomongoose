dde4b87b2881b40bca4682774304e07a944fa357
Rajkumar <rajkumarrajkumar061@gmail.com>
2024-08-08 16:08:02 +0000

    findbyId

--- myApp.js
+++ myApp.js
@@ -43,7 +43,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
